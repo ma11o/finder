92f1b269f06572b878ba7dd70a15c9d2cd60f361
loksam <loksam.jp@gmail.com>
2018-11-22 00:02:11 +0900

    Add change directory

--- bin/index.js
+++ bin/index.js
@@ -1,5 +1,40 @@
 #!/usr/bin/env node
 
 const list = require("../lib/list");
+const keypress = require('keypress');
+const ansiEscapes = require('ansi-escapes');
 
-list.list();
+process.stdout.write(ansiEscapes.clearScreen);
+
+var item = list.list();
+var posX = item.length;
+
+keypress(process.stdin);
+
+process.stdin.on('keypress', function (ch, key) {
+
+    if (key.name == 'up') {
+        process.stdout.write(ansiEscapes.cursorUp(1));
+        posX--;
+    } else if (key.name == 'down') {
+        process.stdout.write(ansiEscapes.cursorDown(1));
+        posX++;
+    } else if (key.name == 'return') {
+
+        if (item[posX].name == '..') {
+            process.chdir('../');
+        } else {
+            process.chdir('./' + item[posX].name);
+        }
+
+        process.stdout.write(ansiEscapes.clearScreen);
+        item = list.list(process.cwd());
+        posX = item.length;
+
+    } else if (key && key.ctrl && key.name == 'c') {
+        process.stdin.pause();
+    }
+});
+
+process.stdin.setRawMode(true);
+process.stdin.resume();
